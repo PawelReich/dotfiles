dcf419088eedd1b4f3182074170d9fb0cac09a58
Pawel Reich <preich@internships.antmicro.com>
2025-11-19 12:33:07 +0100

    nvim: fzf-lua to search through hidden files

--- .config/nvim/lua/gtm/plugins/fzf-lua.lua
+++ .config/nvim/lua/gtm/plugins/fzf-lua.lua
@@ -1,4 +1,10 @@
 return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        grep = {
+            hidden = true,
+            cmd = "rg --column --line-number --no-heading --color=always --smart-case --max-columns=4096 --glob '!.git/'",
+        }
+    }
 }
