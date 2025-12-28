22883fc85d8fdada1e9291994179a2be9797c39b
Pawel Reich <thegitman@outlook.com>
2025-12-29 00:11:42 +0100

    nvim: fzf symbols

--- .config/nvim/lua/gtm/core/keymaps.lua
+++ .config/nvim/lua/gtm/core/keymaps.lua
@@ -50,5 +50,6 @@ map("i", "<C-k>", "<nop>")
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find git-files in cwd" })
 vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua git_status<cr>", { desc = "Git status" })
 vim.keymap.set("n", "<leader>fs", "<cmd>FzfLua live_grep_native<cr>", { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fw", "<cmd>FzfLua lsp_live_workspace_symbols<CR>", { desc = "Find symbols" })
 
 vim.keymap.set("n", "s", "<cmd>HopChar2<cr>", { desc = "Hop" })
