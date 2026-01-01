6f99e40b5526fc3768d5f1a18344045cc7126f10
Pawel Reich <thegitman@outlook.com>
2026-01-01 23:23:47 +0100

    theme: switched to moonfly

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,4 +18,19 @@ if status is-interactive
 
     set -gx EDITOR nvim
 
+    set FZF_DEFAULT_OPTS "--color bg:#080808 \
+  --color bg+:#262626 \
+  --color border:#2e2e2e \
+  --color fg:#b2b2b2 \
+  --color fg+:#e4e4e4 \
+  --color gutter:#262626 \
+  --color header:#80a0ff \
+  --color hl+:#f09479 \
+  --color hl:#f09479 \
+  --color info:#cfcfb0 \
+  --color marker:#f09479 \
+  --color pointer:#ff5189 \
+  --color prompt:#80a0ff \
+  --color spinner:#36c692"
+
 end
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,4 @@ require("gtm.core")
 require("gtm.lazy")
 
 require("bufferline").setup{}
+vim.api.nvim_command("colorscheme moonfly")
