db3623bc3ac211a831eaf993b7e7ed8c22352f77
Pawel Reich <thegitman@outlook.com>
2026-01-02 13:21:23 +0100

    nvim: keymaps cleanup

--- .config/nvim/lua/gtm/core/keymaps.lua
+++ .config/nvim/lua/gtm/core/keymaps.lua
@@ -1,8 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
--- Enable mappings
-
 local map = vim.keymap.set
 
 map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
@@ -28,10 +26,18 @@ map("i", "<C-e>", "<End>", { desc = "move end of line" })
 map("n", "<Tab>","<cmd>BufferLineCycleNext<CR>", {desc = "jump to next buffer" })
 map("n", "<S-Tab>","<cmd>BufferLineCyclePrev<CR>", {desc = "jump to prev buffer" })
 
-map("n", "<C-n>", "<cmd>Neotree position=float toggle=true reveal=true<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+map("n", "<C-n>", "<cmd>Neotree position=float toggle=true reveal=true<CR>", { desc = "Toggle file explorer" })
 
+map("i", "<C-j>", "<nop>")
+map("i", "<C-k>", "<nop>")
 
--- Open lazygit in new tmux window
+vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find git-files in cwd" })
+vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua git_status<cr>", { desc = "Git status" })
+vim.keymap.set("n", "<leader>fs", "<cmd>FzfLua live_grep_native<cr>", { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fw", "<cmd>FzfLua lsp_live_workspace_symbols<CR>", { desc = "Find symbols" })
+
+vim.keymap.set("n", "s", "<cmd>HopChar2<cr>", { desc = "Hop" })
+
 vim.keymap.set(
   "n",
   "<leader>lg",
@@ -41,15 +47,20 @@ vim.keymap.set(
   { desc = "Lazygit" }
 )
 
--- Disable mappings
-local nomap = vim.keymap.del
-map("i", "<C-j>", "<nop>")
-map("i", "<C-k>", "<nop>")
+local function git_helper(action)
+  local script_path = vim.fn.expand("~/.local/git_helper.fish")
+  
+  local buffer_name = vim.fn.expand("%:p") 
+  local cursor_line = vim.fn.line(".")
 
+  local cmd = string.format("fish %s %s %s %s", script_path, action, buffer_name, cursor_line)
 
-vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find git-files in cwd" })
-vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua git_status<cr>", { desc = "Git status" })
-vim.keymap.set("n", "<leader>fs", "<cmd>FzfLua live_grep_native<cr>", { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fw", "<cmd>FzfLua lsp_live_workspace_symbols<CR>", { desc = "Find symbols" })
+  local output = vim.fn.system(cmd)
 
-vim.keymap.set("n", "s", "<cmd>HopChar2<cr>", { desc = "Hop" })
+  if output and output ~= "" then
+    vim.notify(output, vim.log.levels.INFO)
+  end
+end
+
+vim.keymap.set("n", "<leader>gy", function() git_helper("copy-url") end, { desc = "" })
+vim.keymap.set("n", "<leader>go", function() git_helper("goto-issue") end, { desc = "Helix Script: Goto Issue" })
