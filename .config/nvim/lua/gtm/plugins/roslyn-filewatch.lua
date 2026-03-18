b8e478f77fc1ed980e0fa8b35f4731c6064f97d8
Pawel Reich <thegitman@outlook.com>
2026-03-18 22:29:51 +0100

    nvim: Add roslyn-filewatch

---
+++ .config/nvim/lua/gtm/plugins/roslyn-filewatch.lua
@@ -0,0 +1,7 @@
+return {
+  "khoido2003/roslyn-filewatch.nvim",
+  build = "nvim -l build.lua --", -- Compiles or downloads the Native Rust module fallback
+  config = function()
+    require("roslyn_filewatch").setup()
+  end,
+}
