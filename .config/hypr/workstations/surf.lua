717fc29d0a2e64036722f99d9664a7a39c8a05be
Pawel Reich <thegitman@outlook.com>
2026-05-26 00:40:49 +0200

    hypr: Add custom surf config

---
+++ .config/hypr/workstations/surf.lua
@@ -0,0 +1,20 @@
+hl.monitor({
+    output = "",
+    mode = "highres",
+    position = "auto",
+    scale = 1,
+})
+
+
+hl.monitor({
+    output = "eDP-1",
+    mode = "highres",
+    position = "auto",
+    scale = 1.6,
+})
+
+hl.config({
+   input = {
+        sensitivity = 0,
+   }
+})
