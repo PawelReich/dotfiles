bbe42bef73e4420c6ff6a402529cd43fd5179f15
Pawel Reich <preich@antmicro.com>
2026-08-19 22:13:10 +0200

    tmux: Drop configuration

--- .config/fish/functions/tmux_scratch.fish
+++
@@ -1,7 +0,0 @@
-function tmux_scratch --description "tmux popup" --argument-names session_name command
-    if [ "$(tmux display-message -p -F "#{session_name}")" = $argv[1] ]
-        tmux detach-client
-    else
-        tmux popup -w80% -h80% -E "tmux attach -t $argv[1] || tmux new -s $argv[1] $argv[2]"
-    end
-end
